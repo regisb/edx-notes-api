8ee54c7498be6207da96b1e2c31d16d0d0f7453f
Régis Behmo <regis@behmo.com>
2024-10-17 23:03:41 +0200

    feat: meilisearch backend for notes search
    This is a very simple and basic backend. It is based on Django signals,
    just like the Elasticsearch backend. But it is much simpler, in the
    sense that there are just two signals: one for saving documents and one
    for deletion.
    
    This backend is limited, in the sense that it does not support
    highlighting -- but that's probably not such a big deal.
    
    To start using this backend, define the following settings:
    
    	ES_DISABLED = True
    	MEILISEARCH_ENABLED = True
    	MEILISEARCH_URL = "http://meilisearch:7700"
    	MEILISEARCH_API_KEY = "s3cr3t"
    	MEILISEARCH_INDEX = "tutor_student_notes"

--- notesapi/v1/views/__init__.py
+++ notesapi/v1/views/__init__.py
@@ -13,7 +13,10 @@ def get_views_module():
     Import views from either mysql or elasticsearch backend
     """
     if settings.ES_DISABLED:
-        from . import common as backend_module
+        if getattr(settings, "MEILISEARCH_ENABLED", False):
+            from . import meilisearch as backend_module
+        else:
+            from . import common as backend_module
     else:
         from . import elasticsearch as backend_module
     return backend_module
